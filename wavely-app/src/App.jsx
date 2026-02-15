fb961bd81bf172612ee7a30bbded025d7f3ca2e3
chioma <okunborabigail@gmail.com>
2026-02-15 23:37:14 +0000

    Created components folder and initial component files

--- wavely-app/src/App.jsx
+++ wavely-app/src/App.jsx
@@ -1,12 +1,20 @@
-import "./App.css";
+import React from 'react';
+import Hero from './components/Hero';
+import Features from './components/FeatureCard';
+import MusicPlayerPreview from './components/MusicPlayerPreview';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="bg-[#F9F8F6] min-h-screen flex flex-col">
+      <Hero />
+      <Features />
+      <MusicPlayerPreview />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
